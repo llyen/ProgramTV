bb8e3d532906670a11319677c49bc69f4ef9f744
Jakub Wawrzyniak <jakub@itpeople.pl>
2011-06-08 20:25:36 +0200

    simple routing and .htaccess

--- index.php
+++ index.php
@@ -4,7 +4,23 @@ require_once 'system/class.settings.php';
 require_once 'system/class.database.php';
 require_once 'system/abstract.class.element.php';
 require_once 'system/class.item.php';
+require_once 'system/routes.php';
 
 $settings = new Settings();
-//$database = new Database($settings);
-$item = new Item();
+$database = new Database($settings);
+//$item = new Item();
+
+
+?>
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>ProgramTV</title>
+    </head>
+<body>
+    
+    <?php  require_once 'view/'.$route.'.php';  ?>
+    
+</body>
+</html>
--- system/routes.php
+++ system/routes.php
@@ -1,2 +1,6 @@
 <?php
 
+$route = $_GET['r'];
+
+$route = ($route == '') ? 'main' : $route;
+$route = file_exists('view/'.$route.'.php') ? $route : 'main';
