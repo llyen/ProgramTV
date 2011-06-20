25efbc2355c3a694534d8b2f9865e6c2cbbf8cf6
Jakub Wawrzyniak <jakub@itpeople.pl>
2011-06-20 22:00:51 +0200

    channel

--- system/class.channel.php
+++ system/class.channel.php
@@ -2,15 +2,33 @@
 
 class Channel {
     
-    private $channelId;
-    public $channelName;
+    private $database;
     
-    public function __construct(){}
+    public function __construct(Database $database){
+        $this->database = $database;
+    }
     
-    public function addChannel(){}
+    public function add(){
+        
+    }
     
-    public function editChannel(){}
+    public function edit($channelId){
+        Router::redirect('admin_channel_edit', $this->get($channelId));
+    }
     
-    public function deleteChannel(){}
+    public function delete($channelId){
+        
+    }
     
+    public function save($post){}
+    
+    public function update($channelId, $post){}
+    
+    public function remove($channelId){}
+    
+    public function get($channelId){
+        $query = $this->database->query("select channel_name, channel_description from channel where channel_id=$channelId");
+        $channel = $query->fetch();
+        return $channel;
+    }
 }
